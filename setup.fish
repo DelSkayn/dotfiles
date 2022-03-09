be36bf3e9966966821b90db9ce669fd6effca6c7
delskayn <mees.delzenne@gmail.com>
2022-03-09 13:40:28 +0100

    Added some more config files

--- setup.fish
+++ setup.fish
@@ -1,5 +1,10 @@
-if test -d setup.fish
+#!/usr/bin/env fish 
+
+if test ! -e ./setup.fish
 	echo "setup.fish not in current directory please run the script from the dotfiles directory"
 	exit
 end
 ln -s (pwd)/config/nvim ../.config/nvim
+ln -s (pwd)/config/i3 ../.config/i3
+ln -s (pwd)/config/yambar ../.config/yambar
+ln -s (pwd)/config/compton-cfg ../.config/compton-cfg
--- uninstall.fish
+++ uninstall.fish
@@ -1,5 +1,12 @@
-if test -d setup.fish
-	echo "setup.fish not in current directory please run the script from the dotfiles directory"
+#!/usr/bin/env fish 
+
+if test ! -e ./uninstall.fish
+	echo "unsinstall.fish not in current directory please run the script from the dotfiles directory"
 	exit
 end
+
+echo (test -d ./uninstall.fish)
 unlink ../.config/nvim
+unlink ../.config/i3
+unlink ../.config/yambar
+unlink ../.config/compton-cfg
