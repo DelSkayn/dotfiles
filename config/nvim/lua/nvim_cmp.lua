63dac46fb2c76907e2d78b852c65314d8a50fc62
delskayn <mees.delzenne@gmail.com>
2022-05-02 21:27:13 +0200

    Fixed cmp config

--- config/nvim/lua/nvim_cmp.lua
+++ config/nvim/lua/nvim_cmp.lua
@@ -1,5 +1,15 @@
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 local cmp = require("cmp")
 cmp.setup{
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
+    },
     formatting = {
         format = function(entry, vim_item)
             -- fancy icons and a name of kind
@@ -16,41 +26,39 @@ cmp.setup{
             return vim_item
         end,
     },
-    snippet = {
-        expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
-            -- require('luasnip').lsp_expand(args.body)
-        end
-    },
     mapping = {
-        ["<Tab>"] = function(fallback)
+        ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
-        end
-        ,
-        ["<S-Tab>"] = function(fallback)
+        end, {"i","s"}),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
-                cmp.select_previous_item()
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
             else
                 fallback()
             end
-        end
-        ,
+        end, { "i", "s"}),
         ['<CR>'] = cmp.mapping.confirm({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         }),
         ['<C-Space>'] = cmp.mapping.complete(),
     },
-    sources = {
+    sources = cmp.config.sources({
         {name = "nvim_lsp"},
         {name = "buffer"},
         {name = "nvim_lua"},
         {name = "luasnip"},
-    }
+    })
 }
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
